b5a4e5128221a5173c49145112fc00c05f48f319
Ethan <etccustodio@gmail.com>
2025-07-29 04:09:50 -0700

    Updated all the doc strings

--- src/tanglepack/BasePoint.py
+++ src/tanglepack/BasePoint.py
@@ -1,10 +1,45 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import Literal, Optional
 import numpy as np
+
+"""
+Dev Notes:
+
+Consider the data type of _coords more closely. Do I want (2,) or (2,1)?
+"""
 
 
 class BasePoint:
+    """
+    Lowest level data structure for points on a manifold. Doubly linked list connecting
+    points to their next iterate and pre iterate.
 
-    def __init__(self, x=None, y=None, cdist=None, edist=None):
+    Contains methods for inserting, geting, and checking iterates. ie manipulating the
+    linked list.
+
+    Attributes:
+        x (float): x-coordinate of the point.
+        y (float): y-coordinate of the point.
+        _coords (ndarray [2,]): Array coordinates of the point.
+        cdist (float): Canonical distance from the fixed point. Defined as {latex here}
+        edist (float): Arc-length distance from the fixed point.
+        next_iterate (BasePoint): The iterate of the point. Default None.
+        prev_iterate (BasePoint): The pre-iterate of the point. Default None.
+    """
+
+    def __init__(
+        self, x: float = None, y: float = None, cdist: float = None, edist: float = None
+    ):
+        """
+        Initializes a point.
+
+        Args:
+            x (float): x-coordinate of the point. Defaults to None.
+            y (float): y-coordinate of the point. Defaults to None.
+            cdist (float, optional): anonical distance from the fixed point. Defaults to None.
+            edist (float, optional): he pre-iterate of the point. Defaults to None.
+        """
 
         self.x = x
         self.y = y
@@ -12,26 +47,197 @@ class BasePoint:
         self.cdist = cdist
         self.edist = edist
 
+        self.next_iterate = None
+        self.prev_iterate = None
+
         self._set_coords()
 
-    def get_point(self):
+    def get_point(self) -> np.ndarray:
+        """
+        Get the coordinates of the point.
+
+        Returns:
+            np.ndarray: Array of shape (2, ) containing the coordinates of the point.
+        """
 
         return self._coords
 
-    def get_cdist(self, stability: Literal["unstable", "stable"]) -> float:
+    def get_cdist(
+        self, stability: Optional[Literal["unstable", "stable"]] = None
+    ) -> float:
+        """
+        Gets the cdist of the point.
+
+        Args:
+            stability (Literal["unstable", "stable"]): Included here because child class
+                BranchPoint uses this method to distinguish the cdist on from the
+                stable and unstable manifolds. Default to None.
+
+        Returns:
+            float: The canonical distance of the point.
+        """
 
         return self.cdist
 
     def set_x(self, x: float):
+        """
+        Sets the x-coordinate of the point. Also resets the interal _coords.
+
+        Args:
+            x (float): The x-coordinate of the point.
+        """
 
         self.x = x
         self._set_coords()
 
     def set_y(self, y: float):
+        """
+        Sets the y-coordinate of the point. Also resets the internal _coords.
+
+        Args:
+            y (float): The y-coordinate of the point.
+        """
 
         self.y = y
         self._set_coords()
 
     def _set_coords(self):
+        """
+        Sets the coordinate array based on the x and y value of the point.
+        """
 
         self._coords = np.array([self.x, self.y])
+
+    def insert_next_iterate(self, node: BasePoint, num_iterates: int = 1):
+        """
+        Inserts 'node' after this object 'num_iterates' forward
+        in the iterate linked list.
+
+        Args:
+            node (BasePoint): Iterate of the current point to insert into
+                the linked list.
+            num_iterations (int): Number of iterates forward to insert the point.
+
+        Raises:
+            ValueError: Intermediate iterates does not exist.
+            ValueError: The desired iterate does not exist.
+        """
+
+        self.stretch_param = node.stretch_param
+
+        current_node = self
+
+        for _ in range(num_iterates - 1):
+
+            if current_node.next_iterate is None:
+                raise ValueError(f"Itermediate iterates does not exist")
+
+            current_node = current_node.next_iterate
+
+        if current_node.next_iterate is not None:
+            raise ValueError(f"Iterate {num_iterates} already exists")
+
+        current_node.next_iterate = node
+        node.prev_iterate = current_node
+
+    def insert_prev_iterate(self, node: BasePoint, num_iterates: int = 1):
+        """
+        Inserts node before this object 'num_iterates' backwards
+        in the iterate linked list.
+
+        Args:
+            node (BasePoint): Iterate of the current point to insert into
+                the linked list.
+            num_iterations (int): Number of iterates backward to insert the point.
+
+        Raises:
+            ValueError: Intermediate iterates does not exist.
+            ValueError: The desired iterate does not exist.
+        """
+
+        self.stretch_param = node.stretch_param
+
+        current_node = self
+
+        for i in range(num_iterates - 1):
+
+            if current_node.prev_iterate is None:
+                raise ValueError(f"Itermediate iterates do not exist")
+
+            current_node = current_node.prev_iterate
+
+        if current_node.prev_iterate is not None:
+            raise ValueError(f"Pre-iterate {num_iterates} already exists")
+
+        current_node.prev_iterate = node
+        node.next_iterate = current_node
+
+    def get_next_iterate(self, num_iterates: int = 1) -> BasePoint:
+        """
+        Gets the iterate 'num_iterates' forward.
+
+        Args:
+            num_iterates (int): Number of forward iterates.
+
+        Return:
+            BasePoint: The iterate 'num_iterates' forward.
+        """
+
+        current_node = self
+
+        for _ in range(num_iterates):
+
+            if current_node is None:
+                return None
+
+            current_node = current_node.next_iterate
+
+        return current_node
+
+    def get_prev_iterate(self, num_iterates: int = 1) -> BasePoint:
+        """
+        Gets the pre-iterate 'num_iterates' backwards.
+
+        Arge:
+            num_iterates (int): Number of backward iterates.
+
+        Return:
+            BasePoint: The iterate 'num_iterates' backward.
+        """
+
+        current_node = self
+
+        for _ in range(num_iterates):
+
+            if current_node is None:
+                return None
+
+            current_node = current_node.prev_iterate
+
+        return current_node
+
+    def exists_next_iterate(self, num_iterates: int = 1):
+        """
+        Checks if the iterate 'num_iterates' forward exists.
+
+        Args:
+            num_iterates (int): Number of iterates forward to check.
+        """
+
+        if self.get_next_iterate(num_iterates) is None:
+            return False
+        else:
+            return True
+
+    def exists_prev_iterate(self, num_iterates: int = 1):
+        """
+        Checks if the pre-iterate 'num_iterates' backwards exists.
+
+        Args:
+            num_iterations (int): Number of iterates backwards to check.
+        """
+
+        if self.get_prev_iterate(num_iterates) is None:
+            return False
+        else:
+            return True
--- src/tanglepack/Point.py
+++ src/tanglepack/Point.py
@@ -2,24 +2,50 @@ from __future__ import annotations
 import numpy as np
 from .BasePoint import BasePoint
 
+"""
+Dev Notes:
+
+This class is quite tight. Maybe descibe how the only_forward flag 
+work a bit more.
+"""
+
 
 class Point(BasePoint):
+    """
+    Basic functionality of a single point in a manifold.
+    Contains the methods for two doubly linked lists.
+
+    BasePoint implements a linked list to track the iterates.
+        next_iterate, prev_iterate
+    This class implements a linked list to track the geometric ordering.
+        forward, backward
+
+    Attributes:
+        stretch_param (float): Amount by which two points on the manifold separate by
+            upon a single iteration of the map. Usually denoted 'alpha'.
+
+        forward (Point or BasePoint):
+        backward (Point or BasePoint):
+
+        next_iterate (Point or BasePoint):
+        prev_iterate (Point or BasePoint):
+    """
 
     def __init__(self, x=None, y=None, cdist=None, edist=None, stretch_param=None):
         """
         Basic functionality of a single point in a manifold.
         Contains the methods for two doubly linked lists.
 
-        Manifold linked list tracks the ordering of points
+        'Geometrical' linked list tracks the ordering of points
         walking along the manifold the point is on.
 
-        Iterate linked list tracks the ordering of how points
+        'Iterate' linked list tracks the ordering of how points
         map to each other.
 
         Parameters:
-            x (float): x-coordinate of the point
-            y (float): y-coordinate of the point
-            cdist (float): canonical distance from the fixed point
+            x (float): x-coordinate of the point.
+            y (float): y-coordinate of the point.
+            cdist (float): Canonical distance from the fixed point.
         """
 
         super().__init__(x=x, y=y, cdist=cdist, edist=edist)
@@ -29,15 +55,14 @@ class Point(BasePoint):
         self.forward = None
         self.backward = None
 
-        self.next_iterate = None
-        self.prev_iterate = None
-
-    def insert_point_forward(self, node: Point):
+    def insert_point_forward(self, node: Point, only_forward: bool = False):
         """
-        Inserts a point (node) object after this point in the linked list
+        Inserts a point (node) object after this point in the geometric linked list.
 
         Paramters:
-            node (Point): point to be inserted after
+            node (Point): Point to be inserted.
+            only_forward (bool): Flag which switches whether or not we cut off the new
+                point from its current manifold.
         """
 
         if self.forward is node:
@@ -47,18 +72,21 @@ class Point(BasePoint):
             self.stretch_param = node.stretch_param
 
             if self.forward is not None:
-                self.forward.backward = node
-                node.forward = self.forward
+                if not only_forward:
+                    self.forward.backward = node
+                    node.forward = self.forward
 
             self.forward = node
             node.backward = self
 
-    def insert_point_backward(self, node: Point):
+    def insert_point_backward(self, node: Point, only_forward: bool = False):
         """
-        Inserts a point (node) object before this point in the linked list
+        Inserts a point (node) object before this point in the geometric linked list.
 
         Paramters:
-            node (Point): point to be inserted before
+            node (Point): Point to be inserted.
+            only_forward (bool): Flag which switches whether or not we cut off the new
+                point from its current manifold.
         """
 
         if self.backward is node:
@@ -68,38 +96,9 @@ class Point(BasePoint):
             self.stretch_param = node.stretch_param
 
             if self.backward is not None:
-                self.backward.forward = node
-                node.backward = self.backward
+                if not only_forward:
+                    self.backward.forward = node
+                    node.backward = self.backward
 
             self.backward = node
             node.forward = self
-
-    def insert_next_iterate(self, node: Point):
-        """
-        Inserts this object after another point node in the linked list
-
-        Paramters:
-            node (Point): point to be inserted after
-        """
-        self.stretch_param = node.stretch_param
-
-        if self.next_iterate is not None:
-            raise ValueError("next iterate already exists")
-
-        self.next_iterate = node
-        node.prev_iterate = self
-
-    def insert_prev_iterate(self, node: Point):
-        """
-        Inserts this object before another point node in the linked list
-
-        Paramters:
-            node (Point): point to be inserted before
-        """
-        self.stretch_param = node.stretch_param
-
-        if self.prev_iterate is not None:
-            raise ValueError("previous iterate already exists")
-
-        self.prev_iterate = node
-        node.next_iterate = self
